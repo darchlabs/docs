59631fc294837b0728c46e3e8c62305e6cb4b243
Camilo Acuña <camilo.aku@gmail.com>
2023-09-28 03:20:18 +0200

    refactor(page): change landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,11 @@ import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "../HomepageFeatures";
 
 import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+  const { siteConfig, } = useDocusaurusContext();
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
@@ -18,8 +17,15 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro"
+            style={{marginRight: "1rem"}}
           >
-            See Docs
+            Getting Started
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/faq"
+          >
+            FAQs
           </Link>
         </div>
       </div>
@@ -28,15 +34,30 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title="Welcome to DarchLabs">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
+      <main className="container">
+        <section className={styles.features}>
+          <div className={styles.featureItem}>
+            <h2>📚 Libraries</h2>
+            <p>Unlock the potential of DarchLabs with our robust libraries. Crafted for developers, by developers.</p>
+          </div>
+          <div className={styles.featureItem}>
+            <h2>🚀 Our Mission</h2>
+            <p>Revolutionizing the way developers interact with blockchain. Simplifying complexities, one line of code at a time.</p>
+          </div>
+          <div className={styles.featureItem}>
+            <h2>🤝 Partnerships</h2>
+            <p>Collaborating with the best to bring you unparalleled services and tools. Together, we're shaping the future.</p>
+          </div>
+        </section>
+        <section className={styles.partners}>
+          {/* <h2>Our Esteemed Partners</h2>
+          <img className={styles.partnerLogo} src="https://docsearch.algolia.com/img/logos/typescriptlang.svg" alt="Partner 1" />
+          <img className={styles.partnerLogo} src="https://dummyimage.com/640x360/fff/aaa" alt="Partner 2" />
+          <img className={styles.partnerLogo} src="https://dummyimage.com/640x360/fff/aaa" alt="Partner 3" /> */}
+        </section>
       </main>
     </Layout>
   );
